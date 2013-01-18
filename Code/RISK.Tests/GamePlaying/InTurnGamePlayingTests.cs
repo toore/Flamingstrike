7ecc1bb519f6a88a087fc6fade66bd9086625c48
atorebri <andreas.torebring@sogeti.se>
2013-01-18 22:26:19 +0100

    Removed old acceptance test

--- Code/RISK.Tests/GamePlaying/InTurnGamePlayingTests.cs
+++ Code/RISK.Tests/GamePlaying/InTurnGamePlayingTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using NSpec;
-using NUnit.Framework;
 using RISK.Domain;
 using RISK.Domain.Entities;
 using RISK.Domain.EntityProviders;
@@ -76,69 +75,5 @@ namespace RISK.Tests.GamePlaying
         private Game _game;
         private WorldMap _worldMap;
         private ITurn _currentTurn;
-    }
-
-    [TestFixture]
-    public class InTurnGamePlayingTests : GamePlayingTestsBase
-    {
-        [Test]
-        public void Attacking_an_area_and_winning_moves_armies_into_area_and_flags_that_user_should_receive_a_card_when_ending_turn()
-        {
-            Given.
-                new_game_with(humanUsers: 2).
-                first_user_deploys_armies_in_area(NorthAfrica, 5).
-                second_user_deploys_armies_in_available_areas(1).
-                deployment_phase_is_finished();
-
-            When.User.
-                selects_area(NorthAfrica).
-                attacks_area(Brazil).
-                attacker_wins();
-
-            Then.
-                first_user_should_have_armies_in_area(NorthAfrica, 1).
-                first_user_should_have_armies_in_area(Brazil, 4).
-                first_user_should_receive_a_card_when_turn_ends();
-        }
-
-        private InTurnGamePlayingTests first_user_deploys_armies_in_area(IAreaDefinition areaDefinition, int armies)
-        {
-            throw new NotImplementedException();
-        }
-
-        private InTurnGamePlayingTests second_user_deploys_armies_in_available_areas(int armies)
-        {
-            throw new NotImplementedException();
-        }
-
-        private InTurnGamePlayingTests deployment_phase_is_finished()
-        {
-            throw new NotImplementedException();
-        }
-
-        private InTurnGamePlayingTests selects_area(IAreaDefinition areaDefinition)
-        {
-            throw new NotImplementedException();
-        }
-
-        private InTurnGamePlayingTests attacks_area(IAreaDefinition areaDefinition)
-        {
-            throw new NotImplementedException();
-        }
-
-        private InTurnGamePlayingTests attacker_wins()
-        {
-            throw new NotImplementedException();
-        }
-
-        private InTurnGamePlayingTests first_user_should_have_armies_in_area(IAreaDefinition areaDefinition, int armies)
-        {
-            throw new NotImplementedException();
-        }
-
-        private InTurnGamePlayingTests first_user_should_receive_a_card_when_turn_ends()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
