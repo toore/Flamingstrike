ad484e71e8a3eed65d7f48d097fc634270d95332
atorebri <andreas.torebring@sogeti.se>
2013-01-24 21:21:43 +0100

    BattleCalculatorTests

---
+++ Code/RISK.Tests/BattleCalculatorTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using RISK.Domain.Entities;
+using RISK.Domain.GamePlaying;
+using RISK.Domain.GamePlaying.DiceAndCalculation;
+using Rhino.Mocks;
+
+namespace RISK.Tests
+{
+    [TestFixture]
+    public class BattleCalculatorTests
+    {
+        private BattleCalculator _battleCalculator;
+        private IDices _dices;
+        private static readonly IPlayer _attacker = MockRepository.GenerateStub<IPlayer>();
+        private static readonly IPlayer _defender = MockRepository.GenerateStub<IPlayer>();
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dices = MockRepository.GenerateStub<IDices>();
+            _battleCalculator = new BattleCalculator(_dices);
+        }
+
+        private static readonly object[] _attackCases =
+            {
+                new object[] { 2, 1, 0, 1, _attacker, 1, 1 },
+                new object[] { 2, 2, 0, 1, _defender, 2, 1 },
+                new object[] { 2, 2, 1, 0, _defender, 1, 2 },
+            };
+
+        [TestCaseSource("_attackCases")]
+        public void Attack(int attackingArmies, int defendingArmies, int attackerCasualties, int defenderCasualties,
+            IPlayer expectedOwnerAfterAttack, int expectedArmiesInAttackingTerritoryAfter, int expectedArmiesInDefendingTerritoryAfter)
+        {
+            StubDices(attackingArmies, defendingArmies, attackerCasualties, defenderCasualties);
+
+            var attacker = new Territory(new TerritoryLocation("attacker territory", new Continent())) { Owner = _attacker, Armies = attackingArmies };
+            var defender = new Territory(new TerritoryLocation("defender territory", new Continent())) { Owner = _defender, Armies = defendingArmies };
+
+            _battleCalculator.Attack(attacker, defender);
+
+            attacker.Owner.Should().Be(_attacker);
+            attacker.Armies.Should().Be(expectedArmiesInAttackingTerritoryAfter);
+            defender.Owner.Should().Be(expectedOwnerAfterAttack);
+            defender.Armies.Should().Be(expectedArmiesInDefendingTerritoryAfter);
+        }
+
+        private void StubDices(int attackingArmies, int defendingArmies, int attackerCasualties, int defenderCasualties)
+        {
+            var diceResult = MockRepository.GenerateStub<IDicesResult>();
+            diceResult.Stub(x => x.AttackerCasualties).Return(attackerCasualties);
+            diceResult.Stub(x => x.DefenderCasualties).Return(defenderCasualties);
+            _dices.Stub(x => x.Roll(attackingArmies, defendingArmies)).Return(diceResult);
+        }
+    }
+}
