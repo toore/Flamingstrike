f3beb9a4776d120c36ad4249996a0b658046c232
atorebri <andreas.torebring@sogeti.se>
2013-01-31 23:24:06 +0100

    Refactoring styles

---
+++ Code/RISK.Wpf/WorldMap/Territories/TerritoryButtonUserControl.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace RISK.WorldMap.Territories
+{
+    public partial class TerritoryButtonUserControl
+    {
+        public static readonly DependencyProperty PathStyleProperty = DependencyProperty.Register("PathStyle", typeof(Style), typeof(TerritoryButtonUserControl), new PropertyMetadata(default(Style)));
+        public static readonly DependencyProperty TerritoryNameProperty = DependencyProperty.Register("TerritoryName", typeof(string), typeof(TerritoryButtonUserControl), new PropertyMetadata(default(string)));
+
+        public TerritoryButtonUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public Style PathStyle
+        {
+            get { return (Style)GetValue(PathStyleProperty); }
+            set { SetValue(PathStyleProperty, value); }
+        }
+
+        public string TerritoryName
+        {
+            get { return (string)GetValue(TerritoryNameProperty); }
+            set { SetValue(TerritoryNameProperty, value); }
+        }
+    }
+}
