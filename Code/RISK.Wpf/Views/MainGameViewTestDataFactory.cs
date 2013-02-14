51a094b420498b7a046c681e89ea9e0468246a5b
atorebri <andreas.torebring@sogeti.se>
2013-02-14 22:09:55 +0100

    game setup view draft

---
+++ Code/RISK.Wpf/Views/MainGameViewTestDataFactory.cs
@@ -0,0 +1,17 @@
+﻿using GuiWpf.ViewModels;
+
+namespace GuiWpf.Views
+{
+    public class MainGameViewTestDataFactory
+    {
+        private IMainGameViewModel Create()
+        {
+            return new MainGameViewModel(new GameSetupViewModelFactory(null), null, null);
+        }
+
+        public static IMainGameViewModel ViewModel
+        {
+            get { return new MainGameViewTestDataFactory().Create(); }
+        }
+    }
+}
