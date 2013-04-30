13969c8ac11a534aadf5d0ef6ba36c7c650edae7
atorebri <andreas.torebring@sogeti.se>
2013-04-30 13:06:13 +0200

    Armies number update is echoed to display

--- Code/RISK.GuiWpf/ViewModels/Gameplay/Map/TerritoryTextViewModel.cs
+++ Code/RISK.GuiWpf/ViewModels/Gameplay/Map/TerritoryTextViewModel.cs
@@ -3,12 +3,18 @@ using RISK.Domain.Entities;
 
 namespace GuiWpf.ViewModels.Gameplay.Map
 {
-    public class TerritoryTextViewModel : ITerritoryTextViewModel
+    public class TerritoryTextViewModel : ViewModelBase, ITerritoryTextViewModel
     {
         public ILocation Location { get; set; }
 
         public Point Position { get; set; }
         public string TerritoryName { get; set; }
-        public int Armies { get; set; }
+
+        private int _armies;
+        public int Armies
+        {
+            get { return _armies; }
+            set { NotifyOfPropertyChange(value, () => Armies, i => _armies = i); }
+        }
     }
 }
