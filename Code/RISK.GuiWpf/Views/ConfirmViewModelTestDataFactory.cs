b123fff6333473c637dd31105e265ec55c40de84
atorebri <andreas.torebring@sogeti.se>
2013-05-25 21:12:16 +0200

    Confirm view test data

---
+++ Code/RISK.GuiWpf/Views/ConfirmViewModelTestDataFactory.cs
@@ -0,0 +1,22 @@
+﻿using GuiWpf.ViewModels;
+
+namespace GuiWpf.Views
+{
+    public class ConfirmViewModelTestDataFactory
+    {
+        public static ConfirmViewModel ViewModel
+        {
+            get { return new ConfirmViewModelTestDataFactory().Create(); }
+        }
+
+        public ConfirmViewModel Create()
+        {
+            return new ConfirmViewModel(null)
+                {
+                    Message = "Message displays here",
+                    ConfirmText = "Confirm text",
+                    AbortText = "Abort text"
+                };
+        }
+    }
+}
