6729ab9a1e37d9c7b2a90b1b656e28d035556c4b
atorebri <andreas.torebring@gmail.com>
2014-09-29 22:25:06 +0200

    Corrected unit test test data

--- Code/RISK.Tests/Application/Battle/CasualtyEvaluatorTests.cs
+++ Code/RISK.Tests/Application/Battle/CasualtyEvaluatorTests.cs
@@ -7,43 +7,51 @@ namespace RISK.Tests.Application.Battle
 {
     public class CasualtyEvaluatorTests
     {
-        private CasualtyEvaluator _casualtyEvaluator;
-
-        private static readonly object[] _attackerCasualtiesCases =
-            {
-                new object[] { 1, new[] { DiceValue.Two, DiceValue.One, DiceValue.One }, new[] { DiceValue.One, DiceValue.One } },
-                new object[] { 1, new[] { DiceValue.One, DiceValue.One, DiceValue.Two }, new[] { DiceValue.One, DiceValue.One } },
-                new object[] { 1, new[] { DiceValue.Two, DiceValue.One }, new[] { DiceValue.One, DiceValue.One } },
-                new object[] { 1, new[] { DiceValue.One }, new[] { DiceValue.One, DiceValue.One } },
-                new object[] { 1, new[] { DiceValue.One }, new[] { DiceValue.One } },
-                new object[] { 2, new[] { DiceValue.One, DiceValue.One, DiceValue.One }, new[] { DiceValue.One, DiceValue.One } },
-                new object[] { 2, new[] { DiceValue.Six, DiceValue.Six, DiceValue.Six }, new[] { DiceValue.Six, DiceValue.Six } },
-                new object[] { 1, new[] { DiceValue.Six, DiceValue.Six, DiceValue.Six }, new[] { DiceValue.Five, DiceValue.Six } }
-            };
-
-        private static readonly object[] _defenderCasualtiesCases =
-            {
-                new object[] { 1, new[] { DiceValue.Two, DiceValue.One, DiceValue.One }, new[] { DiceValue.One, DiceValue.One } },
-                new object[] { 1, new[] { DiceValue.One, DiceValue.One, DiceValue.Two }, new[] { DiceValue.One, DiceValue.One } },
-                new object[] { 1, new[] { DiceValue.Two, DiceValue.One }, new[] { DiceValue.One, DiceValue.One } },
-                new object[] { 0, new[] { DiceValue.One }, new[] { DiceValue.One, DiceValue.One } },
-                new object[] { 0, new[] { DiceValue.One }, new[] { DiceValue.One } },
-                new object[] { 0, new[] { DiceValue.One, DiceValue.One, DiceValue.One }, new[] { DiceValue.One, DiceValue.One } },
-                new object[] { 0, new[] { DiceValue.Six, DiceValue.Six, DiceValue.Six }, new[] { DiceValue.Six, DiceValue.Six } },
-                new object[] { 1, new[] { DiceValue.Six, DiceValue.Six, DiceValue.Six }, new[] { DiceValue.Five, DiceValue.Six } }
-            };
+        private readonly CasualtyEvaluator _casualtyEvaluator;
 
         public CasualtyEvaluatorTests()
         {
             _casualtyEvaluator = new CasualtyEvaluator();
         }
 
+        public static IEnumerable<object[]> _attackerCasualtiesCases
+        {
+            get
+            {
+                yield return new object[] { 1, new[] { DiceValue.Two, DiceValue.One, DiceValue.One }, new[] { DiceValue.One, DiceValue.One } };
+                yield return new object[] { 1, new[] { DiceValue.One, DiceValue.One, DiceValue.Two }, new[] { DiceValue.One, DiceValue.One } };
+                yield return new object[] { 1, new[] { DiceValue.Two, DiceValue.One }, new[] { DiceValue.One, DiceValue.One } };
+                yield return new object[] { 1, new[] { DiceValue.One }, new[] { DiceValue.One, DiceValue.One } };
+                yield return new object[] { 1, new[] { DiceValue.One }, new[] { DiceValue.One } };
+                yield return new object[] { 2, new[] { DiceValue.One, DiceValue.One, DiceValue.One }, new[] { DiceValue.One, DiceValue.One } };
+                yield return new object[] { 2, new[] { DiceValue.Six, DiceValue.Six, DiceValue.Six }, new[] { DiceValue.Six, DiceValue.Six } };
+                yield return new object[] { 1, new[] { DiceValue.Six, DiceValue.Six, DiceValue.Six }, new[] { DiceValue.Five, DiceValue.Six } };
+            }
+        }
+
+        [Theory]
         [PropertyData("_attackerCasualtiesCases")]
         public void CalculateAttackerCasualties(int expectedCasualties, IEnumerable<DiceValue> attacker, IEnumerable<DiceValue> defender)
         {
             _casualtyEvaluator.GetAttackerCasualties(attacker, defender).Should().Be(expectedCasualties);
         }
 
+        public static IEnumerable<object[]> _defenderCasualtiesCases
+        {
+            get
+            {
+                yield return new object[] { 1, new[] { DiceValue.Two, DiceValue.One, DiceValue.One }, new[] { DiceValue.One, DiceValue.One } };
+                yield return new object[] { 1, new[] { DiceValue.One, DiceValue.One, DiceValue.Two }, new[] { DiceValue.One, DiceValue.One } };
+                yield return new object[] { 1, new[] { DiceValue.Two, DiceValue.One }, new[] { DiceValue.One, DiceValue.One } };
+                yield return new object[] { 0, new[] { DiceValue.One }, new[] { DiceValue.One, DiceValue.One } };
+                yield return new object[] { 0, new[] { DiceValue.One }, new[] { DiceValue.One } };
+                yield return new object[] { 0, new[] { DiceValue.One, DiceValue.One, DiceValue.One }, new[] { DiceValue.One, DiceValue.One } };
+                yield return new object[] { 0, new[] { DiceValue.Six, DiceValue.Six, DiceValue.Six }, new[] { DiceValue.Six, DiceValue.Six } };
+                yield return new object[] { 1, new[] { DiceValue.Six, DiceValue.Six, DiceValue.Six }, new[] { DiceValue.Five, DiceValue.Six } };
+            }
+        }
+
+        [Theory]
         [PropertyData("_defenderCasualtiesCases")]
         public void CalculateDefenderCasualties(int expectedCasualties, IEnumerable<DiceValue> attacker, IEnumerable<DiceValue> defender)
         {
