3cce9f3d0d81af757e2607f4b05bb8066db22267
dirge <andreas.torebring@gmail.com>
2016-03-29 21:56:19 +0200

    Moved commented facts to SendInArmiesToOccupyGameStateTests

--- Code/RISK.Tests/Application/GameStates/AttackGameStateTests.cs
+++ Code/RISK.Tests/Application/GameStates/AttackGameStateTests.cs
@@ -356,55 +356,10 @@ namespace RISK.Tests.Application.GameStates
             act.ShouldThrow<InvalidOperationException>();
         }
 
-        [Fact(Skip = "Not implemented")]
-        public void Can_move_armies_into_captured_territory()
-        {
-            //var defenderIsDefeated = Substitute.For<IBattleResult>();
-            //defenderIsDefeated.IsDefenderDefeated().Returns(true);
-            ////GetAttackCandidatesReturns(_anotherPlayerTerritory);
-            //_battle.Attack(_currentPlayerTerritory, _anotherPlayerTerritory)
-            //    .Returns(defenderIsDefeated);
 
-            //var sut = Create(_gamePlaySetup);
-            //sut.Attack(_currentPlayerRegion, _anotherPlayerRegion);
 
-            //sut.CanSendArmiesToOccupy().Should().BeTrue();
-        }
 
-        [Fact(Skip = "Not implemented")]
-        public void Moves_armies_into_captured_territory()
-        {
-            //var defenderIsEliminated = Substitute.For<IBattleResult>();
-            //defenderIsEliminated.IsDefenderDefeated().Returns(true);
-            ////GetAttackCandidatesReturns(_anotherPlayerTerritory);
-            //_battle.Attack(_currentPlayerTerritory, _anotherPlayerTerritory)
-            //    .Returns(defenderIsEliminated);
-
-            //var sut = Create(_gamePlaySetup);
-            //sut.Attack(_currentPlayerRegion, _anotherPlayerRegion);
-            //sut.SendArmiesToOccupy(3);
-
-            //// Move to own test fixture
-            //// Test that canmove 
-            //// test move
-            //// (test to attack and standard move)
-            //// TBD: test that canmove prevents other actions
-        }
-
-        [Fact(Skip = "Not implemented")]
-        public void Can_not_attack_before_move_into_captured_territory_has_been_confirmed()
-        {
-            //var defenderIsEliminated = Substitute.For<IBattleResult>();
-            //defenderIsEliminated.IsDefenderDefeated().Returns(true);
-            ////GetAttackCandidatesReturns(_anotherPlayerTerritory);
-            //_battle.Attack(_currentPlayerTerritory, _anotherPlayerTerritory)
-            //    .Returns(defenderIsEliminated);
-
-            //var sut = Create(_gamePlaySetup);
-            //sut.Attack(_currentPlayerRegion, _anotherPlayerRegion);
-
-            //sut.AssertCanNotAttack(_currentPlayerRegion, _anotherPlayerRegion);
-        }
+        
 
         protected override IGameState Create(GameData gameData)
         {
