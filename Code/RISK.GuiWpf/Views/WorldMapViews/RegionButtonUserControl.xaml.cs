32cdc146da5bb44c2e7ce03010b1c125ad2187a1
dirge <andreas.torebring@gmail.com>
2016-05-24 01:44:36 +0200

    More renames in gui, from territory to region

---
+++ Code/RISK.GuiWpf/Views/WorldMapViews/RegionButtonUserControl.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace GuiWpf.Views.WorldMapViews
+{
+    public partial class RegionButtonUserControl
+    {
+        public static readonly DependencyProperty GeometryChildrenProperty = DependencyProperty.Register("GeometryChildren", typeof(GeometryCollection), typeof(RegionButtonUserControl), new PropertyMetadata(default(GeometryCollection)));
+        public static readonly DependencyProperty FillColorProperty = DependencyProperty.Register("FillColor", typeof(Color), typeof(RegionButtonUserControl), new PropertyMetadata(default(Color)));
+        public static readonly DependencyProperty StrokeColorProperty = DependencyProperty.Register("StrokeColor", typeof(Color), typeof(RegionButtonUserControl), new PropertyMetadata(default(Color)));
+        public static readonly DependencyProperty MouseOverFillColorProperty = DependencyProperty.Register("MouseOverFillColor", typeof(Color), typeof(RegionButtonUserControl), new PropertyMetadata(default(Color)));
+        public static readonly DependencyProperty MouseOverStrokeColorProperty = DependencyProperty.Register("MouseOverStrokeColor", typeof(Color), typeof(RegionButtonUserControl), new PropertyMetadata(default(Color)));
+
+        public RegionButtonUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public GeometryCollection GeometryChildren
+        {
+            get { return (GeometryCollection)GetValue(GeometryChildrenProperty); }
+            set { SetValue(GeometryChildrenProperty, value); }
+        }
+
+        public Color FillColor
+        {
+            get { return (Color)GetValue(FillColorProperty); }
+            set { SetValue(FillColorProperty, value); }
+        }
+
+        public Color StrokeColor
+        {
+            get { return (Color)GetValue(StrokeColorProperty); }
+            set { SetValue(StrokeColorProperty, value); }
+        }
+
+        public Color MouseOverFillColor
+        {
+            get { return (Color)GetValue(MouseOverFillColorProperty); }
+            set { SetValue(MouseOverFillColorProperty, value); }
+        }
+
+        public Color MouseOverStrokeColor
+        {
+            get { return (Color)GetValue(MouseOverStrokeColorProperty); }
+            set { SetValue(MouseOverStrokeColorProperty, value); }
+        }
+    }
+}
