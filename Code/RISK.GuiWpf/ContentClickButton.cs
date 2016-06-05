86f0635258ba03bf2ac526c81c7a79ee8c28ae61
dirge <andreas.torebring@gmail.com>
2016-06-06 00:50:01 +0200

    Added a content click button, to solve issue with standard button behaviour. Two problems to solve: Placing a button on a Canvas, doesn't seem to give button ActualWidth/ActualHeight and Click event doesn't respect the irregular area of a territory.

---
+++ Code/RISK.GuiWpf/ContentClickButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace GuiWpf
+{
+    public class ContentClickButton : Button
+    {
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            if (e.Handled)
+            {
+                // Handled by ButtonBase, means that IsPressed has been updated. 
+                // We might need to correct, since ButtonBase checks rectangle-size only
+                var isMouseOverChild = IsMouseOverChild(e);
+
+                IsPressed = isMouseOverChild;
+            }
+        }
+
+        private bool IsMouseOverChild(MouseEventArgs e)
+        {
+            var visualChild = GetVisualChild(0);
+            if (visualChild == null)
+            {
+                return true;
+            }
+            var inputElement = (IInputElement)visualChild;
+            var position = e.GetPosition(inputElement);
+
+            var isVisualChildHit = VisualTreeHelper.HitTest(visualChild, position) != null;
+
+            return isVisualChildHit;
+        }
+    }
+}
