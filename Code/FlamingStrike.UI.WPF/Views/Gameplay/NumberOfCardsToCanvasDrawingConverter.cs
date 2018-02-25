df06844833227da5d7750fe3f4fbe540008d9fb6
dirge <andreas.torebring@gmail.com>
2018-02-25 19:49:14 +0100

    Removed gradients, added border to cards "icon" creation.

--- Code/FlamingStrike.UI.WPF/Views/Gameplay/NumberOfCardsToCanvasDrawingConverter.cs
+++ Code/FlamingStrike.UI.WPF/Views/Gameplay/NumberOfCardsToCanvasDrawingConverter.cs
@@ -4,24 +4,26 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Media;
 using System.Windows.Shapes;
 
 namespace FlamingStrike.UI.WPF.Views.Gameplay
 {
-    public class NumberOfCardsToRectanglesConverter : IValueConverter
+    public class NumberOfCardsToCanvasDrawingConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var numberOfCards = (int)value;
 
             return Enumerable.Range(0, numberOfCards)
-                .Select(i => CreateFrameworkElement(i, numberOfCards));
+                .Select(i => CreateFrameworkElement(i, numberOfCards))
+                .ToList();
         }
 
         private static FrameworkElement CreateFrameworkElement(int index, int numberOfCards)
         {
             var left = index * 2.0;
-            var top = index * 0.2;
+            var top = index * 0.25;
             var opacity = GetCardOpacity(index, numberOfCards);
 
             var canvas = new Canvas();
@@ -31,7 +33,7 @@ namespace FlamingStrike.UI.WPF.Views.Gameplay
 
         private static double GetCardOpacity(int index, int numberOfCards)
         {
-            return (index + 1.0) / (numberOfCards + 1);
+            return (index + 1.0) / numberOfCards;
         }
 
         private static Rectangle CreateRectangle(double left, double top, double opacity)
@@ -40,6 +42,7 @@ namespace FlamingStrike.UI.WPF.Views.Gameplay
             rectangle.SetValue(Canvas.LeftProperty, left);
             rectangle.SetValue(Canvas.TopProperty, top);
             rectangle.Opacity = opacity;
+            rectangle.Stroke = Brushes.Black;
 
             return rectangle;
         }
