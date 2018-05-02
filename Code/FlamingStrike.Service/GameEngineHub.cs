fd76b11e282f519065ed7ff46466e023ce0c4318
dirge <andreas.torebring@gmail.com>
2018-05-02 22:45:14 +0200

    Added test service and client

---
+++ Code/FlamingStrike.Service/GameEngineHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace FlamingStrike.Service
+{
+    //[Authorize]
+    public class GameEngineHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("SendAction", "user", "joined");
+
+            //await Clients.All.SendAsync("SendAction", Context.User.Identity.Name, "joined");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Clients.All.SendAsync("SendAction", "user", "left");
+
+            //await Clients.All.SendAsync("SendAction", Context.User.Identity.Name, "left");
+        }
+    }
+}
---
+++ Code/FlamingStrike.TestClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+
+namespace FlamingStrike.TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.ReadKey();
+
+            //var hubConnection = new HubConnection("http://localhost:60643/hubs/gameengine", false);
+
+            var hubConnection = new HubConnectionBuilder()
+                //.WithUrl("http://localhost:60643/hubs/gameengine")
+                .WithUrl("https://localhost:44391/hubs/gameengine")
+                .WithConsoleLogger(LogLevel.Trace)
+                .Build();
+
+            hubConnection.On<string, string>("SendAction", Print);
+            //ServicePointManager.DefaultConnectionLimit = 10;
+
+            hubConnection.StartAsync().Wait();
+
+            Console.ReadKey();
+        }
+
+        private static void Print(string a, string b)
+        {
+            Console.WriteLine($"{a} {b}");
+        }
+    }
+}
